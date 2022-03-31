8811bee057f8f6f9dc9fea64044e71ceada1c3c5
nathan1658 <pooikei165@gmail.com>
2022-03-31 22:36:12 +0800

    - foot tracking enhancement

--- wpf_playground/FootTracking.xaml.cs
+++ wpf_playground/FootTracking.xaml.cs
@@ -31,9 +31,35 @@ namespace wpf_playground
             redBar.Width = 0;
             blueBar.Width = 0;
 
+
+            this.KeyDown += FootTracking_KeyDown;
+            this.Loaded += FootTracking_Loaded;
         }
 
+        private void FootTracking_Loaded(object sender, RoutedEventArgs e)
+        {
+            var window = Window.GetWindow(this);
+            window.KeyDown += FootTracking_KeyDown;
+            window.KeyUp += FootTracking_KeyUp;
+        }
 
+        private void FootTracking_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                isPressed = false;
+            }
+        }
+
+        private void FootTracking_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                isPressed = true;
+            }
+        }
+
+        bool isPressed = false;
         bool inc = true;
         int i = 0;
         Random r = new Random();
@@ -56,14 +82,31 @@ namespace wpf_playground
                         var currentWidth = redBar.Width;
                         if (inc)
                         {
-                            if (currentWidth + INC_VALUE < MAX_WIDTH)
+                            if (currentWidth + INC_VALUE <= MAX_WIDTH)
                                 redBar.Width += INC_VALUE;
                         }
                         else
                         {
-                            if (currentWidth - INC_VALUE > 0)
+                            if (currentWidth - INC_VALUE >= 0)
                                 redBar.Width -= INC_VALUE;
                         }
+
+                        if (isPressed)
+                        {
+                            if (blueBar.Width + INC_VALUE <= MAX_WIDTH)
+                            {
+                                blueBar.Width += INC_VALUE;
+                            }
+                        }
+                        else
+                        {
+                            if (blueBar.Width - INC_VALUE >= 0)
+                            {
+                                blueBar.Width -= INC_VALUE;
+                            }
+                        }
+
+                        debugVal.Text = (blueBar.Width - redBar.Width).ToString();
                     });
                     await Task.Delay(100);
                 }
